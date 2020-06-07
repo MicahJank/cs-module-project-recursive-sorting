b74bf205729b31dfac5c8d158d8153fc9274ecf8
Micah <micahjank@gmail.com>
2020-06-07 16:26:19 -0500

    mvp

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -55,47 +55,65 @@ def merge_sort(arr):
 
 # implement an in-place merge sort algorithm
 def merge_in_place(arr, start, mid, end):
-    
-    i = 0
-    pointer_a = start
-    pointer_b = mid
-    while pointer_a < mid and pointer_b < (end-mid):
-        if arr[pointer_a] < arr[pointer_b]:
-            arr[i] = arr[pointer_a]
-            pointer_a += 1
+
+    # start2 is the position in the array where the second half of the array starts
+    # the first half goes from start up to mid so the second half will be from mid+1 to end
+    start2 = mid + 1
+
+    # arr[mid] is the last element for the first half of the array - arr[start2] is the first element in the second half
+    # I know i am trying to sort the first half and the second half so that what i am left with is a single sorted array
+    # i also know that the array from arr[start]-arr[mid] will be sorted and arr[start2]-arr[end] will be sorted
+    # so with that in mind i can check the last element in the first half with the first element in the second half
+    # if the arr[mid] is less that or equal then i can just return since that must mean that they are sorted already
+    if arr[mid] <= arr[start2]:
+        return
+   
+    # check the two havles of the array and if either of the pointers (start and start2) have reached there respective ends
+    # then the loop will end
+    while start <= mid and start2 <= end:
+        # if the arr position in the first half is less than or equal to the arr in the second half that means we dont
+        # need to change the array because the lesser value is already first
+        if arr[start] <= arr[start2]:
+            # so we can just increase the left half pointer
+            start += 1
         else:
-            arr[i] = arr[pointer_b]
-            pointer_b += 1
+            # if the value in the second half is more than in the first that means we need to get that value moved to the left side
+            # first grab that value that we want to move
+            value = arr[start2]
+            # grab the index of the second half of the array - we can use this when we want to make room on the left side for the new
+            # value
+            index = start2
 
-        i += 1
-    else:
-        if pointer_a < mid:
-            while start < mid:
-                arr[i] = arr[pointer_a]
-                start += 1
-                i += 1
+            # using the index we can move elements over by 1 until we reach the position where we want to replace the value
+            while index != start:
+                arr[index] = arr[index - 1]
+                index -= 1
 
-        if mid < (end-mid):
-            while pointer_b < (end-mid):
-                arr[i] = arr[pointer_b]
-                mid += 1
-                i += 1
-        
-    return arr
+            # once we have reached that position then we can now assign the new value that position
+            arr[start] = value
+            # Update all the pointers
+            start += 1
+            mid += 1
+            start2 += 1
 
 
 def merge_sort_in_place(arr, l, r):
-
-    if (r+1) > 1:
-        mid = int(r/2)
-
+    # the base case for this function since i will be using recursion
+    # i check to see if the left is smaller than the right - if it is it means the recursion has reached a point where
+    # the left pointer and right pointer has overlapped and passed (or are equal) in which case that means the array 
+    # i am trying to sort has a length of 1 which is when the recursion should stop
+    if l < r:
+        # find the middle of the array using the l and r variables
+        mid = l + (r - l)//2
+        # print("mid, left, right", mid, l, r)
+        # first i use recursion to keep halving the left side of the array
         merge_sort_in_place(arr, l, mid)
+        # next halve the right half the array with recursion until its down to 1 item
         merge_sort_in_place(arr, mid+1, r)
-
+        # last can start merging all the pieces of the array together
         merge_in_place(arr, l, mid, r)
-
-
-
+  
+   
     return arr
 
 
